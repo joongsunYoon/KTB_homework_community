488d6eebb3f0ff986739aa8d083c15e9825fc877
JoongsunYoon <yuuu568323@gmail.com>
2026-06-04 22:33:20 +0900

    fix: 잘못된 클래스 사용 수정

--- src/main/java/com/example/community/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/community/global/exception/GlobalExceptionHandler.java
@@ -1,43 +1,43 @@
-package com.example.community.global.exception;
+    package com.example.community.global.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+    import org.springframework.http.HttpStatus;
+    import org.springframework.web.bind.annotation.*;
 
-@RestControllerAdvice
-public class GlobalExceptionHandler {
+    @RestControllerAdvice
+    public class GlobalExceptionHandler {
 
-    //400
-    @ExceptionHandler(IllegalArgumentException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ErrorResponse handleIllegalArgumentException(IllegalArgumentException e) {
-        return new ErrorResponse(e.getMessage() , null);
+        //400
+        @ExceptionHandler(IllegalArgumentException.class)
+        @ResponseStatus(HttpStatus.BAD_REQUEST)
+        public ErrorResponse handleIllegalArgumentException(IllegalArgumentException e) {
+            return new ErrorResponse(e.getMessage() , null);
+        }
+
+        //401
+        @ExceptionHandler(UnauthorizedException.class)
+        @ResponseStatus(HttpStatus.UNAUTHORIZED)
+        public ErrorResponse handleUnauthorizedException(UnauthorizedException e) {
+            return new ErrorResponse(e.getMessage(), e.getData());
+        }
+
+        //403
+        @ExceptionHandler(ForbiddenException.class)
+        @ResponseStatus(HttpStatus.FORBIDDEN)
+        public ErrorResponse handleForbiddenException(ForbiddenException e) {
+            return new ErrorResponse(e.getMessage(),e.getData());
+        }
+
+        //404
+        @ExceptionHandler(NotFoundException.class)
+        @ResponseStatus(HttpStatus.NOT_FOUND)
+        public ErrorResponse handleNotFoundException(NotFoundException e) {
+            return new ErrorResponse(e.getMessage(),e.getData());
+        }
+
+        //500
+        @ExceptionHandler(Exception.class)
+        @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+        public ErrorResponse handleException(Exception e) {
+            return new ErrorResponse(e.getMessage() ,null);
+        }
     }
-
-    //401
-    @ExceptionHandler(UnauthorizedException.class)
-    @ResponseStatus(HttpStatus.UNAUTHORIZED)
-    public ErrorResponse handleUnauthorizedException(UnauthorizedException e) {
-        return new ErrorResponse(e.getMessage(), e.getData());
-    }
-
-    //403
-    @ExceptionHandler(ForbiddenException.class)
-    @ResponseStatus(HttpStatus.FORBIDDEN)
-    public ErrorResponse handleForbiddenException(ForbiddenException e) {
-        return new ErrorResponse(e.getMessage(),e.getData());
-    }
-
-    //404
-    @ExceptionHandler(ForbiddenException.class)
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    public ErrorResponse handleNotFoundException(NotFoundException e) {
-        return new ErrorResponse(e.getMessage(),e.getData());
-    }
-
-    //500
-    @ExceptionHandler(Exception.class)
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public ErrorResponse handleException(Exception e) {
-        return new ErrorResponse(e.getMessage() ,null);
-    }
-}
