1e60c9d1e9a51e92c2074d8f637566cec2555b0a
JoongsunYoon <yuuu568323@gmail.com>
2026-06-08 00:50:05 +0900

    chore: SecurityConfig 파일 위치 변경

--- src/main/java/com/example/community/global/config/SecurityConfig.java
+++ src/main/java/com/example/community/global/config/SecurityConfig.java
@@ -1,6 +1,7 @@
-package com.example.community.global.security;
+package com.example.community.global.config;
 
 import com.example.community.global.exception.ErrorResponse;
+import com.example.community.global.security.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
